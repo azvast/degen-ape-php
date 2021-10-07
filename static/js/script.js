19410afd5d38ebc5c5f32319439c81ab8b67854c
Full-Stack developer <83703580+noveltyz@users.noreply.github.com>
2021-10-07 20:39:57 +0900

    Complete Wallet Connection

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector('nav');
 const menu = document.querySelector(".menu");
-hamburger.addEventListener("click", function(){
+hamburger.addEventListener("click", function () {
     this.classList.toggle("close");
     nav.classList.toggle("reveal");
     menu.classList.toggle("reveal-items");
 });
+
+async function connectWallet(e) {
+    if (typeof window.ethereum == 'undefined') {
+        console.log('MetaMask is not installed!');
+        return;
+    }
+
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+
+    e.target.innerText = account.slice(0, 4) + "..." + account.slice(-4);
+    e.target.style.backgroundColor = "#09cf09";
+}
